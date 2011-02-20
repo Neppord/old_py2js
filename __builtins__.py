74e73294552577d4953c388b5f22cc8ba0c0779f
Samuel Ytterbrink <neppord@gmail.com>
2011-02-20 20:14:40 +0100

    added mor builtin methods onn the Array object

--- __builtins__.py
+++ __builtins__.py
@@ -1,9 +1,44 @@
-eval("""
-    Array.prototype ={};
-    Array.prototype.__getitem__ = function(y){
-      return this[y];
+eval("""Array.prototype ={};
+
+Array.prototype.__init__ = Array;
+
+Array.prototype.__add__ = function(y){
+  return this.concat(y);
+}
+
+Array.prototype.__contains__ = function(y){
+  var i = this.length;
+  while(i--){
+    if(this[i] === y){
+      return true;
     }
-    Array.prototype.__add__ = function(y){
-      return this.concat(y);
-    }
-    """)
+  }
+  return false;
+}
+
+Array.prototype.__delitem__ = function(y){
+  this.splice(y,y);
+}
+
+Array.prototype.__delslice__ = function(i,j){
+  this.splice(i,j);
+  return this;
+}
+
+Array.prototype.__eq__ = function(y){
+  return this === y; //FIXME ?
+}
+
+Array.prototype.__ge__ = function(y){
+  return x >= y;
+}
+
+Array.prototype.__getattribute__ = function(y){
+  return this[y];
+}
+
+Array.prototype.__getitem__ = function(y){
+  return this[y];
+}
+
+""")
